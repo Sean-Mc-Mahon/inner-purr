bb51503b32ad6e5ebf57f712687edfae92cbe380
Sean-Mc-Mahon <sean.mcmahon87@gmail.com>
2021-04-22 13:35:43 +0000

    added stripe elements

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,32 @@
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
+
+/*Code taken from CI course material @: https://learn.codeinstitute.net/courses/course-v1:CodeInstitute+FSF_102+Q1_2020/courseware/4201818c00aa4ba3a0dae243725f6e32/90cda137ebaa461894ba8c89cd83291a/*/
+
+console.log('yo')
+
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripe_public_key);
+var elements = stripe.elements();
+var style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+var card = elements.create('card', {style: style});
+card.mount('#card-element');
--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IMXLgFNfg03GNyIQHPrkUG8nfaGhwLvMtz5Z75HVA9zE0eve8h5K85XGdfhuX8JSaKjNzAM57gf1QNZRoD41umb00fplkMXvt',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
