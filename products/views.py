4e530ef31c6b72571afaf0d7db3d5a34ff42a800
Sean-Mc-Mahon <sean.mcmahon87@gmail.com>
2021-05-13 13:22:28 +0000

    edit/delete buttons added to product card and product details

--- products/views.py
+++ products/views.py
@@ -77,9 +77,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
@@ -116,3 +116,12 @@ def edit_product(request, product_id):
 
     return render(request, template, context)
 #  taken from CI module @ https://github.com/Code-Institute-Solutions/boutique_ado_v1/blob/de5e400ea7efd03403e289435b2b821fd6f5748a/products/views.py
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
+#  taken from CI module @ https://learn.codeinstitute.net/courses/course-v1:CodeInstitute+FSF_102+Q1_2020/courseware/4201818c00aa4ba3a0dae243725f6e32/c557000edc0549b5b372ab66702580b3/
