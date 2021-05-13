54f7d6a4b99da106ee2258255ec364edec30a817
Sean-Mc-Mahon <sean.mcmahon87@gmail.com>
2021-05-13 13:04:25 +0000

    edit product functionality added

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     #  specify that the product ID should be an integer, otherwise navigating to products /add will interpret the string add as a product id.
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
