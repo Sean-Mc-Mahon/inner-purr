10dae957abc95a32cbd41cf3d7583b6e43fb6c8e
Sean-Mc-Mahon <sean.mcmahon87@gmail.com>
2021-05-14 12:53:44 +0000

    removed secret key and set debug

--- purr/settings.py
+++ purr/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r$et9w!z-)c=ra+ams*niq6^k2zhx5!dibr6c4)by2_)@v#0kc'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['inner-purr.herokuapp.com', 'localhost']
 
